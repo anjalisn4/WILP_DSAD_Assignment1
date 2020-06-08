9da3c71953893a0ac8dd51dc0dfd4b854178c947
anjalisn4 <44434447+anjalisn4@users.noreply.github.com>
2020-06-08 14:50:05 +0530

    Create MovieBoxOffice.py
    basic structure of the movie box office

---
+++ MovieBoxOffice.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 16:06:31 2019
+@author: DSAD Group 37
+@contribution:
+    Team Member     Roll Number
+    ===========================
+    Amit Kumar     2019HC04174
+    Pranesh P	2019HC04175
+    Anjali Sunder Naik	2019HC04178
+"""
+
+class MovieBoxOffice:
+    def __init__(self, w, n):
+        self.n = n
+        self.w= w
+        
