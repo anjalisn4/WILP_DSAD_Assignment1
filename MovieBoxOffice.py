6ecc88815d13257138664775a9738f527c13db3f
anjalisn4 <44434447+anjalisn4@users.noreply.github.com>
2020-07-06 19:26:39 +0530

    Update MovieBoxOffice.py

--- MovieBoxOffice.py
+++ MovieBoxOffice.py
@@ -9,9 +9,17 @@ Created on Tue Dec 10 16:06:31 2019
     Pranesh P	2019HC04175
     Anjali Sunder Naik	2019HC04178
 """
-
+"""
+w: Number of box offices --> 3
+n : max length of each queue --> 5
+"""
 class MovieBoxOffice:
     def __init__(self, w, n):
-        self.n = n
-        self.w= w
-        
+            self.n = n
+            self.w = w
+            self.queues = [[None for i in range(n)] for j in range(w)]
+            self.starts = [0 for i in range(w)]
+            self.ends = [0 for i in range(w)]
+            self.open= [False for i in range(w)]
+            self.open[0] = True
+   def isOpen (self, windowId):  
