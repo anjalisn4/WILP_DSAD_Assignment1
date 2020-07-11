05dcd2ee0f8232dacf971c375b895b345fc7500f
Anjali Sunder Naik (RBEI/ETL) <SunderNaik.Anjali@in.bosch.com>
2020-07-11 21:16:13 +0530

    fixed bugs

--- CircularQueue/MovieBoxOffice.py
+++ CircularQueue/MovieBoxOffice.py
@@ -26,10 +26,13 @@ class BoxOffice:
 
     # returns the queue (number of people waiting) in front of the window.
     def getWindow(self, windowid):
+        window =[]
         if self.isOpen(windowid):
-            return [x for x in self.queues[windowid] if x is not None]
-        else:
-            return [] 
+            index= self.starts[windowid]
+            for i in range(self.currlen[windowid]):
+                window.append(self.queues[windowid][index])
+                index= (index+1)%self.n        
+        return [x for x in window if x is not None]        
 
     def size(self, index):
         return self.currlen[index]
@@ -107,11 +110,11 @@ if __name__ == '__main__':
             boxoffice = BoxOffice(w, n)
         if 'isOpen' in i:
             itype, id = i.split(':')
-            isBoxOfficeOpen = boxoffice.isOpen(int(id))
+            isBoxOfficeOpen = boxoffice.isOpen(int(id)-1)
             output.write(STRING_CONCAT % (i, isBoxOfficeOpen))
         if 'getWindow' in i:
             itype, id = i.split(':')
-            waiting_list = boxoffice.getWindow(int(id))
+            waiting_list = boxoffice.getWindow(int(id)-1)
             output.write(STRING_CONCAT % (i, waiting_list))
         if 'addPerson' in i:
             ADD_PERSON_STRING_CONCAT="%s >> w%s\n"
@@ -119,8 +122,9 @@ if __name__ == '__main__':
             addperson = boxoffice.addPerson(int(id))
             if(addperson ==-1):
                 output.write(STRING_CONCAT % (i, "all queues are full"))   
-            else:                 
-                output.write(ADD_PERSON_STRING_CONCAT % (i, addperson))            
+            else:
+                addperson_q = addperson + 1                 
+                output.write(ADD_PERSON_STRING_CONCAT % (i, addperson_q))            
         if 'giveTicket' in i:
             tickets = boxoffice.giveTicket()
             output.write(STRING_CONCAT % (i, tickets))
